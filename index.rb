33771ea6dd29013922362a5d742674c05b496c8b
Leyline <leyline@qq.com>
2018-04-16 00:22:25 +0800

    abstraction

--- index.rb
+++ index.rb
@@ -1,18 +1,14 @@
 require 'sinatra'
 require 'json/ext' # required for .to_json
 require 'mongo'
+require 'tempfile'
 require 'roo'
 require 'roo-xls'
-require 'tempfile'
+
+enable :sessions
 
 configure do
-  db = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'leyline-sheet')
-
-  set :mongo_db, db
-  set :sheets, db[:Sheets]
-  set :heads, db[:Heads]
-  set :users, db[:Users]
-
+  set :mongo,  Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'leyline-sheet')
 end
 
 get "/" do
@@ -21,13 +17,75 @@ end
 
 get '/collections/?' do
   content_type :json
-  settings.mongo_db.database.collection_names.to_json
+  settings.mongo.database.collection_names.to_json
+end
+
+get '/collection/:collection/?' do
+  content_type :json
+  collection = settings.mongo[params[:collection]]
+  collection.find.to_a.to_json
+end
+
+get '/collection/:collection/:id/?' do
+  content_type :json
+  collection = settings.mongo[params[:collection]]
+  find_document_by_id(params[:id],collection).to_json
+end
+
+get '/collection/:collection/:id/populate/:populate?' do
+  content_type :json
+  collection = settings.mongo[params[:collection]]
+  unpopulated = find_document_by_id(params[:id],collection)
+
+  params[:populate].split(',').each {|field_name|
+    field_name = field_name.downcase
+    collection_to_populate = settings.mongo[field_name + 's']
+    id = unpopulated[field_name]
+    unpopulated[field_name] = find_document_by_id(id, collection_to_populate)
+  }
+
+  return unpopulated.to_json
+end
+
+post '/collection/:collection/?' do
+  content_type :json
+  collection = settings.mongo[params[:collection]]
+  result = collection.insert_one JSON.parse(request.body.read)
+  find_document_by_id(result.inserted_id,collection).to_json
+end
+
+put '/collection/:collection/:id/?' do
+  content_type :json
+  collection = settings.mongo[params[:collection]]
+  result = collection.find_one_and_update({:_id=>to_object_id(params[:id])},{'$set' => JSON.parse(request.body.read)})
+  find_document_by_id(result[:_id],collection).to_json
+end
+
+delete '/collection/:collection/:id/?' do
+  content_type :json
+  collection = settings.mongo[params[:collection]]
+  document = collection.find_one_and_delete(:_id=>to_object_id(params[:id]))
+
+  if !document.nil?
+    {:success => true}.to_json
+  else
+    {:success => false}.to_json
+  end
+end
+
+post '/login' do
+  content_type :json
+  users = settings.mongo[:users]
+  res = users.find(:name=>params[:name],:password=>params[:password]).to_a.first
+  unless res.nil?
+    session['userId'] = res['_id']
+    return 200
+  end
+  return 403
 end
 
 helpers do
-  # a helper method to turn a string ID
-  # representation into a BSON::ObjectId
-  def object_id val
+  def to_object_id(val)
     begin
       BSON::ObjectId.from_string(val)
     rescue BSON::ObjectId::Invalid
@@ -35,92 +93,34 @@ helpers do
     end
   end
 
-  def document_by_id (id,collection)
-      id = object_id(id) if String === id
-    if id.nil?
-      {}.to_json
-    else
-      document = collection.find(:_id => id).to_a.first
-      (document || {}).to_json
+  def find_document_by_id (id,collection)
+    id = to_object_id(id) if String === id
+    res = nil
+    unless id.nil?
+      res = collection.find(:_id => id).to_a.first
     end
-  end
-end
-
-# list all documents in the test collection
-get '/sheets/?' do
-  content_type :json
-  settings.sheets.find.to_a.to_json
-end
-
-# find a document by its ID
-get '/sheet/:id/?' do
-  content_type :json
-  sheet_unpopulated = settings.sheets.find(:_id => object_id(params[:id])).to_a.first
-  sheet_unpopulated[:head] = document_by_id(sheet_unpopulated[:head],settings.heads)
-  return sheet_unpopulated.to_json
-end
-
-post '/sheet/?' do
-  content_type :json
-  db = settings.sheets
-  result = db.insert_one params
-  db.find(:_id => result.inserted_id).to_a.first.to_json
-end
-
-# update the document specified by :id, setting its
-# contents to params, then return the full document
-put '/sheet/:id/?' do
-  content_type :json
-  id = object_id(params[:id])
-  settings.sheets.find(:_id => id).
-      find_one_and_update('$set' => request.params)
-  document_by_id(id)
-end
-
-# update the document specified by :id, setting just its
-# name attribute to params[:name], then return the full
-# document
-put '/sheet/:id/:name/?' do
-  content_type :json
-  id   = object_id(params[:id])
-  name = params[:name]
-  settings.sheets.find(:_id => id).
-      find_one_and_update('$set' => {:name => name})
-  document_by_id(id)
-end
-
-# delete the specified document and return success
-delete '/sheet/:id' do
-  content_type :json
-  db = settings.sheets
-  id = object_id(params[:id])
-  documents = db.find(:_id => id)
-  if !documents.to_a.first.nil?
-    documents.find_one_and_delete
-    {:success => true}.to_json
-  else
-    {:success => false}.to_json
+    return (res || {})
   end
 end
 
 post '/sheet/init' do
-    HEAD_ROW_NUM = 1
-    @filename = params[:file][:filename]
-    file = params[:file][:tempfile]
-    filetype = File.extname(@filename) == '.xls' ? :xls : :xlsx
-    raw = Roo::Spreadsheet.open(file.path, extension: filetype)
+  HEAD_ROW_NUM = 1
+  @filename = params[:file][:filename]
+  file = params[:file][:tempfile]
+  filetype = File.extname(@filename) == '.xls' ? :xls : :xlsx
+  raw = Roo::Spreadsheet.open(file.path, extension: filetype)
 
-    sheet = raw.sheet(0)
-    head_names = sheet.row(HEAD_ROW_NUM)
-    head_content_type = sheet.row(HEAD_ROW_NUM+1)
+  sheet = raw.sheet(0)
+  head_names = sheet.row(HEAD_ROW_NUM)
+  head_content_type = sheet.row(HEAD_ROW_NUM+1)
 
-    head_content = head_names.each_with_index.map{|head_name,index|{head_name=>head_content_type[index]}}.to_a
+  head_content = head_names.each_with_index.map{|head_name,index|{head_name=>head_content_type[index]}}.to_a
 
-    heads_collection = settings.heads
-    result = heads_collection.insert_one :content => head_content
-    # heads_collection.find(:_id => result.inserted_id).to_a.first.to_json
+  heads_collection = settings.mongo[:heads]
+  result = heads_collection.insert_one :content => head_content
 
-    sheets_collection = settings.sheets
-    result =  sheets_collection.insert_one({:head => result.inserted_id,:title => (@filename.sub! '.'+filetype.to_s, "") })
-    sheets_collection.find(:_id => result.inserted_id).to_a.first.to_json
+  sheets_collection = settings.mongo[:sheets]
+  result =  sheets_collection.insert_one({:head => result.inserted_id,:title => (@filename.sub! '.'+filetype.to_s, "") })
+  find_document_by_id(result.inserted_id,sheets_collection).to_json
 end
+
